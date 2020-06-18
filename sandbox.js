ace1364a5bcb3c09f78fb6070565c87abbacc308
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-06-17 23:57:30 -0400

    Trying reverse geocode

--- sandbox.js
+++ sandbox.js
@@ -1,60 +1,40 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    var geocoder;
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-  } 
-  //Get the latitude and the longitude;
-  function successFunction(position) {
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-      codeLatLng(lat, lng)
-  }
-  
-  function errorFunction(){
-      alert("Geocoder failed");
-  }
-  
-    function initialize() {
-     geocoder = new google.maps.Geocoder();
-  
-  
-  
-    }
-  
-    function codeLatLng(lat, lng) {
-  
-      var latlng = new google.maps.LatLng(lat, lng);
-      geocoder.geocode({'latLng': latlng}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-        console.log(results)
-          if (results[1]) {
-           //formatted address
-           alert(results[0].formatted_address)
-          //find country name
-               for (var i=0; i<results[0].address_components.length; i++) {
-              for (var b=0;b<results[0].address_components[i].types.length;b++) {
-  
-              //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-                  if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-                      //this is the object you are looking for
-                      city= results[0].address_components[i];
-                      break;
-                  }
-              }
+    const KEY = "";
+    const LAT = 50.1;
+    const LNG = -97.3;
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=${KEY}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let parts = data.results[0].address_components;
+        document.body.insertAdjacentHTML(
+          "beforeend",
+          `<p>Formatted: ${data.results[0].formatted_address}</p>`
+        );
+        parts.forEach(part => {
+          if (part.types.includes("country")) {
+            //we found "country" inside the data.results[0].address_components[x].types array
+            document.body.insertAdjacentHTML(
+              "beforeend",
+              `<p>COUNTRY: ${part.long_name}</p>`
+            );
           }
-          //city data
-          alert(city.short_name + " " + city.long_name)
-  
-  
-          } else {
-            alert("No results found");
+          if (part.types.includes("administrative_area_level_1")) {
+            document.body.insertAdjacentHTML(
+              "beforeend",
+              `<p>PROVINCE: ${part.long_name}</p>`
+            );
           }
-        } else {
-          alert("Geocoder failed due to: " + status);
-        }
-      });
-    }
+          if (part.types.includes("administrative_area_level_3")) {
+            document.body.insertAdjacentHTML(
+              "beforeend",
+              `<p>LEVEL 3: ${part.long_name}</p>`
+            );
+          }
+        });
+      })
+      .catch(err => console.warn(err.message));
 
 });
